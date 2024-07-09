d05d0522e044769284577efc44ebc087822e0737
Nejc Medved <medo.nejc@gmail.com>
2024-07-09 08:45:11 +0200

    - added dummy terminal

---
+++ web-app/src/layouts/TerminalLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+
+defineOptions({
+  name: 'TerminalLayout'
+});
+
+</script>
---
+++ web-app/src/pages/TerminalPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <div id="terminal"></div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import {onMounted} from 'vue';
+import * as xterm from 'xterm';
+
+defineOptions({
+  name: 'TerminalPage'
+});
+
+onMounted(() => {
+  console.log('mounting terminal')
+  const term = new xterm.Terminal();
+  const element = document.getElementById('terminal')
+  if (element) {
+    term.open(element);
+    term.write('$')
+  }
+
+})
+
+</script>
+
+<style src='/node_modules/xterm/css/xterm.css'>
+    /* global styles */
+</style>
